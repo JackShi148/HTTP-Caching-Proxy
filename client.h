d2c7aced54d297f5e3396b2c1f09517c164ad478
Elexxiu <1604875799@qq.com>
2023-02-13 22:34:49 -0500

    request part

---
+++ client.h
@@ -0,0 +1,50 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <cstring>
+#include <netdb.h>
+#include <unistd.h>
+#include <vector>
+#include <cstdlib>
+
+#include "response.h"
+#include "assert.h"
+#include "exception.h"
+
+using namespace std;
+
+class Client {
+public:
+    int status;
+    int socket_fd;
+    const char * hostname;
+    const char * port;
+    struct addrinfo host_info;
+    struct addrinfo * host_info_list;
+
+    Client(const char * hostname, const char * port) : status(0), socket_fd(0), hostname(hostname), port(port), host_info_list(NULL) {
+        initClient();
+    }
+
+    ~Client() {
+        if(socket_fd != 0) {
+            close(socket_fd);
+        }
+        if(host_info_list != NULL) {
+            free(host_info_list);
+        }
+    }
+
+    void initClient();
+    void clientSend(vector<char> &msg);
+    void clientRecv(vector<char> &msg);
+};
+
+void validateMsg(int msg_len);
+void recvHelper(int socket_fd, vector<char> &msg);
+void sendHelper(int socket_fd, vector<char> &msg);
+
+
+#endif
