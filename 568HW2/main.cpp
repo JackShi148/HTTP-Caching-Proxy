96003293b18f895a6104db196d88c065a41e3489
Elexxiu <1604875799@qq.com>
2023-02-14 18:47:15 -0500

    delete

--- 568HW2/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "client.h"
-
-int main() {
-    Client c = Client("127.0.0.1", "123");
-    std::cout << "Client has connected with hostname: " << c.hostname << " and port: " << c.port << std::endl; 
-    return 0;
-}
