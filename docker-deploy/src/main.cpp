e7bdc8e40bc51b245e48c4e3a5431dc567a21a12
Elexxiu <1604875799@qq.com>
2023-02-28 10:31:18 -0500

    finalize

--- docker-deploy/src/main.cpp
+++ docker-deploy/src/main.cpp
@@ -1,9 +1,14 @@
+#include <unistd.h>
+
 #include "proxy.h"
 
 int main()
 {
   try
   {
+    if(daemon(1, 0) != 0) {
+      throw Exception("Error: fail to init as daemon");
+    }
     Proxy p("12345");
     p.startProxy();
   }
@@ -11,5 +16,5 @@ int main()
   {
     std::cerr << e.what() << std::endl;
   }
-  return 1;
+  return EXIT_FAILURE;
 }
